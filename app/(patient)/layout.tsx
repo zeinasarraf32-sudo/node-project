58dd18c12f99114a46cd0c9e3e5e7353f0004a34
zeina <zeinasarraf32@gmail.com>
2026-09-03 22:56:56 +0300

    'pull3sept'

--- app/(patient)/layout.tsx
+++ app/(patient)/layout.tsx
@@ -8,13 +8,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="min-h-screen bg-slate-50 antialiased overflow-x-hidden">
+    
+     <>
         <Navbar />
         <Providers>
           {children}
         </Providers>
-      </body>
-    </html>
+  
+   </>
   );
 }
--- app/api/doctors/[id]/route.ts
+++ app/api/doctors/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
-import { DoctorStatus } from '@prisma/client';
+
 import { IDoctor } from '@/interfaces/interfaces';
 
 export async function PUT(
